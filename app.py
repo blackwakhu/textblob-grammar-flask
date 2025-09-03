ea7cb9f4a26cf2aeefe8f4ce10530b0a39c0a8fc
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2025-09-03 20:04:28 +0300

    reduced some unneccessary code

--- app.py
+++ app.py
@@ -5,57 +5,16 @@ from flask_restful import Resource, Api, reqparse
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/')
-def homepage():
+# @app.route('/spelling/grammar', methods=['GET','POST'])
+# def grammar():
+# 	if request.method == 'POST':
+# 		wrong = request.form.get('word')
+# 		correct = TextBlob(wrong).correct()
+# 	return render_template('spell.html', correct=correct)
+
+@app.route("/")
+def index():
 	return render_template('index.html')
-
-@app.route('/dictionary')
-def dictionary():
-	return render_template('diction.html')
-
-@app.route('/dictionary/meaning', methods=['GET','POST'])
-def meaningDictionary():
-	if request.method == 'POST':
-		word_search = request.form.get('word')
-		meaning = Word(word_search).definitions
-	return render_template('diction.html', meaning=meaning)
-
-@app.route('/spelling')
-def spelling():
-	return render_template('spell.html')
-
-@app.route('/spelling/grammar', methods=['GET','POST'])
-def grammar():
-	if request.method == 'POST':
-		wrong = request.form.get('word')
-		correct = TextBlob(wrong).correct()
-	return render_template('spell.html', correct=correct)
-
-@app.route('/plural and singular')
-def plural():
-	return render_template('plurl.html')
-
-@app.route('/plural and singular/plural', methods=['GET', 'POST'])
-def plural1():
-	if request.method == 'POST':
-		sing = TextBlob(request.form.get('word'))
-		plur = sing.words.pluralize()
-	return render_template('plurl.html', plurals=f'the plural of {sing} is {plur[0]}')
-
-@app.route('/plural and singular/singular', methods=['GET', 'POST'])
-def singular():
-	if request.method == 'POST':
-		sing = TextBlob(request.form.get('word'))
-		plur = sing.words.singularize()
-	return render_template('plurl.html', singulars=f'the plural of {sing} is {plur[0]}')
-
-@app.route("/oxford")
-def oxford():
-	return render_template('oxford.html')
-
-@app.route("/hello")
-def hello():
-	return {"greet": "hello world"}
 
 word = reqparse.RequestParser()
 word.add_argument("word", type=str, help="Word to search", required=True)
@@ -65,9 +24,8 @@ class Meaning(Resource):
 	def post(self):
 		args = word.parse_args()
 		return {
-			"meaning": Word(args['word']).definitions
+			"meaning": Word(args['word']).definitions,
 		}
-
-
+	
 if __name__ == '__main__':
 	app()
